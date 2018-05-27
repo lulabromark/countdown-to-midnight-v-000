805232d5ba08bf5ebf6fd85eac75caff4eeebfd9
Mark Lawrence <lawrence.ml@gmail.com>
2018-05-27 17:05:59 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,13 +2,14 @@
 
 def countdown(number)
   while number > 0
-    countdown_with_sleep(1)
+    
     number -= 1
     puts "#{number} SECOND(S)!"
+    countdown_with_sleep(1)
   end
   return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
-  sleep (number)
+  sleep(number)
 end
